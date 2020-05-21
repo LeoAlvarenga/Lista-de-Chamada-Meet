6a6528da059fb064ea32a82b23697acc4386df7d
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-05-21 14:57:16 -0300

    coments

--- content.js
+++ content.js
@@ -1,13 +1,15 @@
 console.log("Working");
 
+//Array to save the names 
 const list = new Array();
 
+//wait fo the element appears and get the names 
 const interval = setInterval(() => {
     let names = document.getElementsByClassName("cS7aqe");
     console.log(names);
 
     if (names.length > 0) {
-
+        //check if the names are already saved, then saves it
         for (let name of names) {
             console.log(name.textContent);
 
@@ -22,7 +24,7 @@ const interval = setInterval(() => {
 }, 1000);
 
 
-
+//listen to the message from the popup and gives it a response
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg === 'getData') {
         console.log("message received");
