e9101a2ce70a1988714b4804360e2164931bfc8b
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-05-21 14:09:14 -0300

    Persist names

--- content.js
+++ content.js
@@ -1,39 +1,45 @@
 console.log("Working");
-// const list = new Array();
-// const interval = setInterval(() => {
-//     let names = document.getElementsByClassName("cS7aqe");
-//     console.log(names);
 
-//     if(names.length > 0){
+const list = new Array();
 
-//         for(let name of names){
-//             console.log(name.textContent);
+const interval = setInterval(() => {
+    let names = document.getElementsByClassName("cS7aqe");
+    console.log(names);
 
-//             list.includes(name.textContent) ? null : list.push(name.textContent);
-           
-//         }
+    if (names.length > 0) {
 
-//         console.log(list);
+        for (let name of names) {
+            console.log(name.textContent);
 
-//         let options = {
-//             active: true,
-//             currentWindow: true
-//         }
+            if (!list.includes(name.textContent)) {
+                localStorage.setItem("aluno"+localStorage.length, name.textContent);
+                list.push(name.textContent);
+            }
 
-//         chrome.tabs.query(options, gotTabs);
-        
-//     }
-    
-// },1000)
+        }
+    }
+
+}, 1000);
+
+
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if(msg === 'getData') {
+    if (msg === 'getData') {
         console.log("message received");
-        const elements = [...document.getElementsByClassName('cS7aqe')];
+        // const elements = [...document.getElementsByClassName('cS7aqe')];
+        const elements = [];
+        setList(elements)
         console.log(elements);
-        const texts = elements.map(el => el.textContent);
-        const uniqTexts = [...new Set(texts)];
+        //const texts = elements.map(el => el.textContent);
+        const uniqTexts = [...new Set(elements)];
         sendResponse(uniqTexts);
     }
 })
 
+function setList(list) {
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.getItem("aluno" + i)) {
+            list.push(localStorage.getItem("aluno" + i));
+        }
+    }
+}
