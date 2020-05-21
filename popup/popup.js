2e807e182585bce4e4bb053528c5334831be2b57
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-05-21 13:25:15 -0300

    Comment on code

--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,20 @@
-// for(let name of list){
-
-//     chrome.browserAction.getPopup(() => {
-//         let ul = document.getElementById("List");
-//         let li = document.createElement("li");
-//         li.innerHTML = name;
-//         ul.appendChild(li);
-//     })
-        
-//     }
-
+// array of already received names
 const arrNames = [];
 
+//query to send messages to all active windows with a setInterval
 chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
     setInterval(ping, 1000, tab);
 });
 
+//function executade in setInterval
 function ping(tab) {
     console.log("sending");
+    //send messages to all tabs and wait for a response
     chrome.tabs.sendMessage(tab.id, 'getData', res => {
         console.log(res);
         const ul = document.querySelector("#chamada");
         console.log(ul);
+        //take the response and check the names already received before, and then render the new names
         res.map(el => {
             if(!arrNames.includes(el)){
                 arrNames.push(el);
